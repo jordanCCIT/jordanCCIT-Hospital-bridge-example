a671d4fe41d738e2ebe77d4c45e798cdf32421b0
BenWilliams <deanpbr@gmail.com>
2018-02-07 10:23:37 -0500

    Receptionist added

---
+++ src/main/java/high_st_hospital/Receptionist.java
@@ -0,0 +1,15 @@
+package high_st_hospital;
+
+public class Receptionist extends Employee {
+
+	public Receptionist(String empName, String empNumber) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getSalary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/test/java/high_st_hospital/ReceptionistTest.java
+++ src/test/java/high_st_hospital/ReceptionistTest.java
@@ -1,5 +1,29 @@
 package high_st_hospital;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class ReceptionistTest {
+	@Test
+	public void shouldAnswerPhone() {
+		Employee underTest = new Receptionist("Barb", "1141");
+	}
+
+	@Test
+	public void shouldHaveAName() {
+		assertThat(underTest.getName(), is(DOCTOR_NAME));
+	}
+
+	@Test
+	public void shouldHaveANumber() {
+		assertThat(underTest.getNumber(), is(DOCTOR_EMPLOYEE_NUMBER));
+	}
+
+	@Test
+	public void shouldHaveASalary() {
+		assertThat(underTest.getSalary(), is(90000));
+	}
 
 }
