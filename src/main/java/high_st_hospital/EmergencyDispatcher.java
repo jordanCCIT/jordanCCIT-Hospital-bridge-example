178453f141500896e05d0ec8cb103562f1e180a1
jordanCCIT <jordanadposting@gmail.com>
2020-12-11 08:56:45 -0500

    Addition of Receptionist and Emerg. Dispatch

--- src/main/java/high_st_hospital/EmergencyDispatcher.java
+++ src/main/java/high_st_hospital/EmergencyDispatcher.java
@@ -1,5 +1,18 @@
 package high_st_hospital;
 
-/*public class EmergencyDispatcher extends Receptionist implements MedicalDuties {
+public class EmergencyDispatcher extends Receptionist implements MedicalDuties {
 
-}*/
+    public EmergencyDispatcher(String name, String employeeNumber) {
+        super(name, employeeNumber);
+    }
+
+    @Override
+    public void careForPatient(Patient patient) {
+        patient.improveHealth(5);
+    }
+
+    @Override
+    public void drawBlood(Patient patient) {
+        patient.giveBlood(5);
+    }
+}
--- src/main/java/high_st_hospital/Receptionist.java
+++ src/main/java/high_st_hospital/Receptionist.java
@@ -1,5 +1,29 @@
 package high_st_hospital;
 
-/*public class Receptionist extends Employee {
+public class Receptionist extends Employee {
 
-}*/
+    private boolean isAnsweringPhones;
+
+    public Receptionist(String name, String employeeNumber){
+        this.empName = name;
+        this.empNumber = employeeNumber;
+    }
+
+    @Override
+    public int getSalary() {
+        return 45000;
+    }
+
+    public void answeringPhones(Hospital specificHospital){
+        isAnsweringPhones = true;
+    }
+
+    public boolean getIsAnsweringPhones(){
+        return isAnsweringPhones;
+    }
+
+    @Override
+    public String toString(){
+        return empName + "\t" + empNumber + "\t" + isAnsweringPhones;
+    }
+}
